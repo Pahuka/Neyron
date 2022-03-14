857354682a4d9fbed27369f51f5137d27f742c64
Mo Morf <J-a-c-k@yandex.ru>
2022-03-15 00:31:54 +0500

    Добавил HP, голод, смену цвета при жизненном цикле, смерть.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,15 +14,26 @@ namespace Neyron
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Pixel> pixels = new List<Pixel>();
-        static Grid myGrid;
+        Dictionary<int, Pixel> pixels = new Dictionary<int, Pixel>();
+        Grid myGrid;
         System.Windows.Threading.DispatcherTimer myTimer = new System.Windows.Threading.DispatcherTimer();
 
         public MainWindow()
         {
             InitializeComponent();
+            addPixel.IsEnabled = false;
             myTimer.Tick += Move;
-            myTimer.Interval = new TimeSpan(0, 0, 0, 0, 50);
+            myTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+        }
+
+        private void ChangeColor(Pixel pixel, bool isHungry)
+        {
+            var color = ((SolidColorBrush)pixel.Show().Fill).Color;
+            if (color.G > 0)
+            {
+                var gCanal = isHungry ? (byte)(color.G - color.G / pixel.Healh) : (byte)(color.G + (byte)pixel.Healh);
+                pixel.Show().Fill = new SolidColorBrush(Color.FromRgb(color.R, gCanal, color.B));
+            }
         }
 
         private void CreateGrid(int x, int y)
@@ -31,7 +42,7 @@ namespace Neyron
             {
                 foreach (var pixel in pixels)
                 {
-                    myGrid.Children.Remove(pixel.Show());
+                    myGrid.Children.Remove(pixel.Value.Show());
                 }
                 mainGrid.Children.Remove(myGrid);
             }
@@ -46,29 +57,75 @@ namespace Neyron
 
             foreach (var pixel in pixels)
             {
-                myGrid.Children.Add((UIElement)pixel.Show());
+                myGrid.Children.Add((UIElement)pixel.Value.Show());
             }
 
             for (int i = 0; i < x; i++)
             {
-                myGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(20) });
+                myGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(25) });
             }
             for (int j = 0; j < y; j++)
             {
-                myGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(20) });
+                myGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(25) });
             }
             this.SizeToContent = SizeToContent.WidthAndHeight;
             mainGrid.Children.Add(myGrid);
         }
 
+        private void Hunt(Pixel pixel)
+        {
+            for (int x = -1; x < 2; x++)
+            {
+                for (int y = -1; y < 2; y++)
+                {
+                    if (x == 0 & y == 0)
+                        continue;
+                    var tempPixel = pixels.Where(p => p.Value.X == x & p.Value.Y == y).FirstOrDefault().Value;
+                    if (tempPixel != null && tempPixel.Show().Fill != Brushes.Black)
+                    {
+                        Eat(pixel, tempPixel);
+                        pixel.X += x;
+                        pixel.Y += y;
+                    }
+                }
+            }
+        }
+
+        private void Eat(Pixel pixel1, Pixel pixel2)
+        {
+            if (pixel1.Id != pixel2.Id)
+            {
+                if (pixel1.Healh > pixel2.Healh)
+                {
+                    pixel1.Healh += pixel2.Healh;
+                    ChangeColor(pixel1, false);
+                    pixel2.Healh = 0;
+                    pixel2.ChangeColor(Brushes.Black);
+                }
+                else
+                {
+                    pixel2.Healh += pixel1.Healh;
+                    ChangeColor(pixel2, false);
+                    pixel1.Healh = 0;
+                    pixel1.ChangeColor(Brushes.Black);
+                }
+            }
+        }
+
         private void CalculateMove(Pixel pixel)
         {
             var randomPos = new Random();
+            Hunt(pixel);
             var x = pixel.X + randomPos.Next(-1, 2);
             var y = pixel.Y + randomPos.Next(-1, 2);
             if (x >= 0 & x <= myGrid.RowDefinitions.Count)
                 if (y >= 0 & y <= myGrid.ColumnDefinitions.Count)
                 {
+                    var tempPixel = pixels.Where(p => p.Value.X == x & p.Value.Y == y).FirstOrDefault().Value;
+                    if (tempPixel != null && tempPixel.Show().Fill != Brushes.Black)
+                        Eat(pixel, tempPixel);
+                    //pixel.Healh--;
+                    //ChangeColor(pixel, true);
                     pixel.X = x;
                     pixel.Y = y;
                 }
@@ -80,9 +137,21 @@ namespace Neyron
 
             foreach (var pixel in pixels)
             {
-                CalculateMove(pixel);
-                Grid.SetColumn((UIElement)pixel.Show(), pixel.Y);
-                Grid.SetRow((UIElement)pixel.Show(), pixel.X);
+                if (pixel.Value.Show().Fill != Brushes.Black)
+                    CalculateMove(pixel.Value);
+            }
+
+            foreach (var pixel in pixels)
+            {
+                pixel.Value.Healh--;
+                ChangeColor(pixel.Value, true);
+                if (pixel.Value.Healh <= 0 && pixel.Value.Show().Fill != Brushes.Black)
+                    pixel.Value.Show().Fill = Brushes.Black;
+                if (pixel.Value.Show().Fill != Brushes.Black)
+                {
+                    Grid.SetColumn((UIElement)pixel.Value.Show(), pixel.Value.Y);
+                    Grid.SetRow((UIElement)pixel.Value.Show(), pixel.Value.X);
+                }
             }
         }
 
@@ -91,16 +160,19 @@ namespace Neyron
             myTimer.Stop();
             pixels.Clear();
             CreateGrid(int.Parse(sizeX.Text), int.Parse(sizeY.Text));
+            addPixel.IsEnabled = true;
         }
 
         private void addPixel_Click(object sender, RoutedEventArgs e)
         {
-            pixels.Add(new Pixel(new Ellipse()
+            var random = new Random();
+            var pixel = new Pixel(new Ellipse()
             {
-                Height = 20,
-                Width = 20,
-                Fill = Brushes.Red
-            }, 0, 0));
+                Height = 25,
+                Width = 25,
+                Fill = new SolidColorBrush(Color.FromRgb(0, 255, 33))
+            }, random.Next(0, myGrid.ColumnDefinitions.Count), random.Next(0, myGrid.RowDefinitions.Count));
+            pixels.Add(pixel.Id, pixel);
 
             myTimer.Start();
         }
--- Pixel.cs
+++ Pixel.cs
@@ -1,21 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Media;
 using System.Windows.Shapes;
 
 namespace Neyron
 {
     class Pixel
     {
+        private static int GlobalId { get; set; }
         private Ellipse form;
+        public int Healh { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
+        public int Id { get; set; }
 
         public Pixel(Ellipse form, int x, int y)
         {
             this.form = form;
             X = x;
             Y = y;
+            GlobalId++;
+            Id = GlobalId;
+            Healh = new Random().Next(100, 201);
+        }
+
+        public void ChangeColor(Brush color)
+        {
+            form.Fill = color;
         }
 
         public Ellipse Show()
